6fced0190364c887a6d303af39f41b7c739fc6e4
Rethabile Diale <rethadiale27@gmail.com>
2024-06-04 20:15:29 +0200

    Added nested routes for HostVanDetail component with tabs for Info, Pricing, and Photos

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,14 @@ export default function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
