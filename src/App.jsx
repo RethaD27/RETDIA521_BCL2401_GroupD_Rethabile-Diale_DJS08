6bf1c98397447c034d17a1989e5fd8c7d56771bf
Rethabile Diale <rethadiale27@gmail.com>
2024-06-04 15:17:07 +0200

    Setup routing for the application with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-// import Navbar from "./components/Navbar.jsx";
-import Vans from "./pages/Vans/Vans.jsx";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetail";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
 
 import "./server";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vans" element={<Vans />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
